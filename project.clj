edca964f4dbf7a8ff014797abb0745bb8d8710c6
jsa-aerial <jsa.aerial@gmail.com>
2019-10-17 16:47:51 -0400

    bump version and clojure dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject aerial.fs "1.1.2"
+(defproject aerial.fs "1.1.6"
   :description "General file system utiltity functions"
   :url "https://github.com/jsa-aerial/aerial.fs"
   :license {:name "The MIT License (MIT)"
             :url  "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :scm {:name "git"
 	:url "https://github.com/jsa-aerial/aerial.fs"}
   :aot :all)
